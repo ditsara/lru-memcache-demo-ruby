4f1e2c6bf2ee327d8c077cc26d2b51d8ec8bc76f
Dan Itsara <dan.itsara@gmail.com>
2017-01-29 16:14:32 +0700

    mem limit

--- lib/lru/memcache/demo.rb
+++ lib/lru/memcache/demo.rb
@@ -4,25 +4,63 @@ module Lru
   module Memcache
     module Demo
       class Cache
-        attr_reader :mem_limit
+        attr_reader :mem_limit, :head, :tail, :size
 
         def initialize(mem_limit: 100)
           @mem_limit = mem_limit
           @lookup = {}
           @head = nil
+          @tail = nil
+          @size = 0
         end
 
         def store(key, val)
+          @lookup[key] ? replace(key, val) : create(key, val)
+        end
+
+        def replace(key, val)
+          elem = @lookup[key]
+          elem.val = val
+        end
+
+        def create(key, val)
           new_element = Element.new(key, val)
+          old_head = @head
 
           # attach new element to previous head
-          new_element.next_elem = @head
-          @head.prev_elem = new_element if @head
+          new_element.next_elem = old_head
+          old_head.prev_elem = new_element if old_head
+
+          # set tail pointer if necessary
+          @tail = new_element if new_element.next_elem.nil?
+          @tail = old_head if old_head && old_head.next_elem.nil?
 
           # now move the head "pointer" var
           @head = new_element
 
+          @size += 1
+          shrink
           @lookup[key] = new_element
+        end
+
+        def pop
+          return unless @tail
+
+          # remove from lookup table
+          @lookup.delete @tail.key
+
+          # attach "pointer" to new tail and remove reference to old tail
+          @tail = @tail.prev_elem
+          @tail.next_elem = nil if @tail
+          @size -= 1
+
+          @tail
+        end
+
+        def shrink
+          while (size > mem_limit)
+            pop
+          end
         end
 
         # Attempts to retrieve the value at key. Returns nil
@@ -31,8 +69,10 @@ module Lru
           found_element = @lookup[key]
 
           if found_element
+            prev_elem = found_element.prev_elem
             Element.swap found_element.prev_elem, found_element
             @head = found_element if found_element.prev_elem.nil?
+            @tail = prev_elem if prev_elem&.next_elem.nil?
             found_element.read
           end
         end
@@ -49,6 +89,15 @@ module Lru
       end
 
       class Element
+        def self.remove(elem)
+          return unless elem
+          before_elem = elem.prev_elem
+          after_elem = elem.next_elem
+
+          before_elem.next_elem = after_elem if before_elem
+          after_elem.prev_elem = before_elem if after_elem
+        end
+
         def self.swap(elem_a, elem_b)
           # no action if element is already head or tail of the list
           return true unless elem_a && elem_b
--- spec/lru/memcache/demo_spec.rb
+++ spec/lru/memcache/demo_spec.rb
@@ -7,13 +7,18 @@ RSpec.describe Lru::Memcache::Demo do
 
   describe 'cache' do
     before(:each) do
-      Cache = Lru::Memcache::Demo::Cache
-      @cache = Cache.new
+      Cache ||= Lru::Memcache::Demo::Cache
+      @mem_limit = 5
+      @cache = Cache.new mem_limit: @mem_limit
     end
 
     it 'retrieves stored values' do
       @cache.store 'x', 'y'
       expect(@cache.retrieve 'x').to eq('y')
+    end
+
+    it 'returns nil for values not found' do
+      expect(@cache.retrieve 'not_there').to be_nil
     end
 
     context 'with stored elements' do
@@ -39,6 +44,32 @@ RSpec.describe Lru::Memcache::Demo do
       it 'promotes element order when retrieving tail' do
         @cache.retrieve 'first'
         expect(@cache.list.to_a).to eq(%w(third first second))
+      end
+    end
+
+    context 'size' do
+      it 'increments size when element added' do
+        expect { @cache.store 'k', 'v' }.to change { @cache.size }.from(0).to(1)
+      end
+
+      it 'does not change size when element replaced' do
+        @cache.store 'k', 'v'
+        expect { @cache.store 'k', 'v' }.not_to change { @cache.size }
+      end
+
+      it 'decrements size when element removed' do
+        @cache.store 'k', 'v'
+        expect { @cache.pop }.to change { @cache.size }.by(-1)
+      end
+
+      it 'drops elements if mem limit reached' do
+        keys = @mem_limit.times.map { |i| "k-#{i}" }
+        keys.each { |k| @cache.store k, 'val'}
+
+        expect { @cache.store 'last', 'val' }.not_to change { @cache.size }
+
+        list = (keys + ['last']).reverse[0..-2]
+        expect(@cache.list.to_a).to eq(list)
       end
     end
   end
