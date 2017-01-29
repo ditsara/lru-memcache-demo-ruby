a5d9e1dca255d5fdaf93b4d562a4d4147d44d3f0
Dan Itsara <dan.itsara@gmail.com>
2017-01-29 13:38:21 +0700

    elem list

--- lib/lru/memcache/demo.rb
+++ lib/lru/memcache/demo.rb
@@ -3,7 +3,97 @@ require "lru/memcache/demo/version"
 module Lru
   module Memcache
     module Demo
-      # Your code goes here...
+      class Cache
+        attr_reader :mem_limit
+
+        def initialize(mem_limit: 100)
+          @mem_limit = mem_limit
+          @lookup = {}
+          @head = nil
+        end
+
+        def store(key, val)
+          new_element = Element.new(key, val)
+
+          # attach new element to previous head
+          new_element.next_elem = @head
+          @head.prev_elem = new_element if @head
+
+          # now move the head "pointer" var
+          @head = new_element
+
+          @lookup[key] = new_element
+        end
+
+        # Attempts to retrieve the value at key. Returns nil
+        # if it does not exist.
+        def retrieve(key)
+          found_element = @lookup[key]
+
+          if found_element
+            Element.swap found_element.prev_elem, found_element
+            @head = found_element if found_element.prev_elem.nil?
+            found_element.read
+          end
+        end
+
+        def list
+          Enumerator.new do |y|
+            current = @head
+            while(current)
+              y.yield current.key
+              current = current.next_elem
+            end
+          end
+        end
+      end
+
+      class Element
+        def self.swap(elem_a, elem_b)
+          # no action if element is already head or tail of the list
+          return true unless elem_a && elem_b
+
+          # ensure elem_a and elem_b are actually neighbors
+          return unless elem_a.next_elem == elem_b
+          return unless elem_b.prev_elem == elem_a
+
+          before_elem = elem_a.prev_elem
+          after_elem = elem_b.next_elem
+
+          before_elem.next_elem = elem_b if before_elem
+          elem_b.prev_elem = before_elem
+          elem_b.next_elem = elem_a
+          elem_a.prev_elem = elem_b
+          elem_a.next_elem = after_elem
+          after_elem.prev_elem = elem_a if after_elem
+
+          true
+        end
+
+        attr_accessor :key, :val
+        attr_accessor :next_elem, :prev_elem
+        attr_reader :accessed_at
+
+        def initialize(key, val)
+          @key = key
+          @val = val
+          touch
+        end
+
+        def read
+          touch
+          val
+        end
+
+        def touch
+          @accessed_at = Time.now
+        end
+
+        def debug
+          { key: key, next: next_elem&.key, prev: prev_elem&.key}
+        end
+      end
+
     end
   end
 end
--- spec/lru/memcache/demo_spec.rb
+++ spec/lru/memcache/demo_spec.rb
@@ -5,7 +5,41 @@ RSpec.describe Lru::Memcache::Demo do
     expect(Lru::Memcache::Demo::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'cache' do
+    before(:each) do
+      Cache = Lru::Memcache::Demo::Cache
+      @cache = Cache.new
+    end
+
+    it 'retrieves stored values' do
+      @cache.store 'x', 'y'
+      expect(@cache.retrieve 'x').to eq('y')
+    end
+
+    context 'with stored elements' do
+      before(:each) do
+        @cache.store 'first', 'data'
+        @cache.store 'second', 'data'
+        @cache.store 'third', 'data'
+      end
+      it 'pushes new vals to head' do
+        expect(@cache.list.to_a).to eq(%w(third second first))
+      end
+
+      it 'promotes element order on retrieve' do
+        @cache.retrieve 'second'
+        expect(@cache.list.to_a).to eq(%w(second third first))
+      end
+
+      it 'leaves element order unchanged when retrieving head' do
+        @cache.retrieve 'third'
+        expect(@cache.list.to_a).to eq(%w(third second first))
+      end
+
+      it 'promotes element order when retrieving tail' do
+        @cache.retrieve 'first'
+        expect(@cache.list.to_a).to eq(%w(third first second))
+      end
+    end
   end
 end
